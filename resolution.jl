73a43ce41688725780fe4afa13cdf2efdd592d2a
Kaputchino <andreasmulard@gmail.com>
2025-05-14 14:57:12 +0200

    bon cplex marche c deja ca

--- resolution.jl
+++ resolution.jl
@@ -61,11 +61,14 @@ function cplexSolve(grid::Matrix{Int})
 
     # Solve the model
     optimize!(model)
+    B_bool = [value(blacked[i,j]) > 0.5 for i in 1:size(blacked, 1), j in 1:size(blacked, 2)]
 
+    print_black_only(B_bool)
     # Return:
     # 1 - true if an optimum is found
     # 2 - the resolution time
-    return MOI.get(model, MOI.TerminationStatus()) == MOI.FEASIBLE_POINT || MOI.get(model, MOI.TerminationStatus()) == MOI.OPTIMAL, time() - start, blacked
+    # 3 - the grid of blacked element
+    return MOI.get(model, MOI.TerminationStatus()) == MOI.FEASIBLE_POINT || MOI.get(model, MOI.TerminationStatus()) == MOI.OPTIMAL, time() - start, B_bool
     
 end
 
@@ -88,16 +91,16 @@ Remark: If an instance has previously been solved (either by cplex or the heuris
 """
 function solveDataSet()
 
-    dataFolder = "../data/"
-    resFolder = "../res/"
+    dataFolder = "./data/"
+    resFolder = "./res/"
     #TODO know true size
 
 
     
     
     # Array which contains the name of the resolution methods
-    resolutionMethod = ["cplex"]
-    #resolutionMethod = ["cplex", "heuristique"]
+    #resolutionMethod = ["cplex"]
+    resolutionMethod = ["cplex", "heuristique"]
 
     # Array which contains the result folder of each resolution method
     resolutionFolder = resFolder .* resolutionMethod
@@ -165,7 +168,7 @@ function solveDataSet()
                         println("In file resolution.jl, in method solveDataSet(), TODO: fix heuristicSolve() arguments and returned values")
                         
                         # Solve it and get the results
-                        isOptimal, resolutionTime = heuristicSolve()
+                        solved_blacked,isOptimal = solveByHeuristic(grid)
 
                         # Stop the chronometer
                         resolutionTime = time() - startingTime
@@ -196,4 +199,178 @@ function solveDataSet()
             println(resolutionMethod[methodId], " time: " * string(round(solveTime, sigdigits=2)) * "s\n")
         end         
     end 
+end
+
+# Vérifie si une position est dans la grille
+function inbounds(i, j, h, w)
+    return 1 ≤ i ≤ h && 1 ≤ j ≤ w
+end
+
+# Vérifie si deux noirs sont adjacents
+function validate_no_adjacent_blacks(black)
+    h, w = size(black)
+    for i in 1:h, j in 1:w
+        if black[i, j]
+            for (di, dj) in ((0, 1), (1, 0), (0, -1), (-1, 0))
+                ni, nj = i + di, j + dj
+                if inbounds(ni, nj, h, w) && black[ni, nj]
+                    println("Erreur: cases noires adjacentes à ($i,$j) et ($ni,$nj)")
+                    return false
+                end
+            end
+        end
+    end
+    return true
+end
+
+# Propagation : si une case est blanche, toutes les autres identiques dans ligne/colonne → noires
+function propagate_blanks(grid, black)
+    h, w = size(grid)
+    changed = false
+    for i in 1:h, j in 1:w
+        if !black[i, j]
+            val = grid[i, j]
+            # Ligne
+            for jj in 1:w
+                if jj != j && grid[i, jj] == val && !black[i, jj]
+                    black[i, jj] = true
+                    changed = true
+                end
+            end
+            # Colonne
+            for ii in 1:h
+                if ii != i && grid[ii, j] == val && !black[ii, j]
+                    black[ii, j] = true
+                    changed = true
+                end
+            end
+        end
+    end
+    return changed
+end
+
+# Détecte et noircit automatiquement si un chiffre apparaît ≥2 fois et seul 1 peut rester
+function resolve_duplicates(grid, black)
+    h, w = size(grid)
+    changed = false
+    # Lignes
+    for i in 1:h
+        counts = Dict{Int, Vector{Int}}()
+        for j in 1:w
+            val = grid[i, j]
+            counts[val] = get(counts, val, Int[])  # default empty
+            push!(counts[val], j)
+        end
+        for (val, js) in counts
+            visibles = filter(j -> !black[i, j], js)
+            if length(visibles) == 1
+                for j in js
+                    if j != visibles[1] && !black[i, j]
+                        black[i, j] = true
+                        changed = true
+                    end
+                end
+            end
+        end
+    end
+    # Colonnes
+    for j in 1:w
+        counts = Dict{Int, Vector{Int}}()
+        for i in 1:h
+            val = grid[i, j]
+            counts[val] = get(counts, val, Int[])
+            push!(counts[val], i)
+        end
+        for (val, is) in counts
+            visibles = filter(i -> !black[i, j], is)
+            if length(visibles) == 1
+                for i in is
+                    if i != visibles[1] && !black[i, j]
+                        black[i, j] = true
+                        changed = true
+                    end
+                end
+            end
+        end
+    end
+    return changed
+end
+
+# Flood-fill pour vérifier connexité des blancs
+function flood_fill(grid, black)
+    h, w = size(grid)
+    visited = falses(h, w)
+    found = false
+    for i in 1:h, j in 1:w
+        if !black[i, j]
+            queue = [(i, j)]
+            visited[i, j] = true
+            found = true
+            break
+        end
+    end
+    if !found
+        return false
+    end
+
+    while !isempty(queue)
+        (ci, cj) = popfirst!(queue)
+        for (di, dj) in ((0,1), (1,0), (0,-1), (-1,0))
+            ni, nj = ci + di, cj + dj
+            if inbounds(ni, nj, h, w) && !black[ni, nj] && !visited[ni, nj]
+                visited[ni, nj] = true
+                push!(queue, (ni, nj))
+            end
+        end
+    end
+
+    # Vérifie s'il reste des blancs non visités → ils sont isolés
+    for i in 1:h, j in 1:w
+        if !black[i, j] && !visited[i, j]
+            println("Erreur: case blanche isolée à ($i,$j)")
+            return false
+        end
+    end
+    return true
+end
+
+# Boucle principale de résolution simple
+function solveByHeuristic(grid)
+    h, w = size(grid)
+    black = falses(h, w)
+    isOptimal = true
+    iteration = 0
+    while true
+        iteration += 1
+        println("Itération $iteration...")
+        changed = false
+        changed |= resolve_duplicates(grid, black)
+        changed |= propagate_blanks(grid, black)
+        if !changed
+            break
+        end
+    end
+
+    if !validate_no_adjacent_blacks(black)
+        println("Grille invalide (noirs adjacents)")
+        isOptimal = false
+    elseif !flood_fill(grid, black)
+        println("Grille invalide (blancs non connectés)")
+        isOptimal = false
+
+    else
+        println("Grille valide")
+    end
+
+    return black,isOptimal
+end
+
+function print_black_only(black::Matrix{Bool})
+    h, w = size(black)
+    for i in 1:h
+        for j in 1:w
+            print(black[i, j] ? "X " : ". ")
+        end
+        println()
+    end
 end
